c8cef45e2dd94bee18c9d27efd115ef828f113c0
siviotti <douglas.siviotti@gmail.com>
2018-10-02 23:06:37 -0300

    commit 2 /out e alteracoes

--- src/Lib.kt
+++ src/Lib.kt
@@ -3,3 +3,5 @@ package lib
 fun add(a: Int, b: Int): Int{
     return a + b
 }
+
+fun mymaxOf(a: Int, b : Int) = if (a > b) a else b
--- src/Main.kt
+++ src/Main.kt
@@ -4,5 +4,6 @@ import lib.*
 
 fun main (args: Array<String>){
     val c = add(2, 3)
+    println(mymaxOf(3,4))
 
 }
